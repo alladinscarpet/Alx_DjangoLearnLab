a9878d06a022252b9de63ae3866555401242f1a4
alladinscarpet <dmw3ndwa@gmail.com>
2025-12-16 19:43:42 +0300

    wk 14: task 4 - form tagging update

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -5,6 +5,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from .models import Post, Comment
+from taggit.forms import TagWidget
 
 
 # Use the project's current User model, whether it's the default Django User or a custom one
@@ -28,14 +29,17 @@ class ProfileForm(forms.ModelForm):
 
 # ModelForm for create/edit posts
 class PostForm(forms.ModelForm):
-    '''We exclude author and published_date from the form; author will be set in the view.'''
+    '''
+    Form for creating and editing blog posts.
+    Uses django-taggit's TagWidget for tag input.
+    We exclude author and published_date from the form; author will be set in the view.'''
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
         widgets = {
             'title': forms.TextInput(attrs={'placeholder': 'Post title'}),
             'content': forms.Textarea(attrs={'rows': 10}),
-            'tags': forms.CheckboxSelectMultiple(),  # Use checkbox select for multiple tags
+            'tags': TagWidget(),  # Use checkbox select for multiple tags
         }
 
 
